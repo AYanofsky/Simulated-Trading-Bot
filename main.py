a8f2899e6cd2795c59f18e97fce2ab37c249ed1d
Andrew Yanofsky <andrewdyanofsky@gmail.com>
2025-04-07 13:42:27 -0400

    restructured for dataloader. again.

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
-from tradingbot.utils import get_tickers_from_file
-from tradingbot.dataloader import dataloader
-
 def main():
-    tickers = get_tickers_from_file()
+    # get tickers from a file
 
     # returns multindex dataframe
-    raw = dataloader(tickers)
 
 if __name__ == '__main__':
     main()
--- tradingbot/dataloader.py
+++ tradingbot/dataloader.py
@@ -3,122 +3,38 @@
 """
 DATABASE STRUCTURE DURING DATABASE COLLECTION
 
-data {
+preprocessed {
 Stock: varchar(6),
 High Price: int,
 Low Price: int,
 Close Price: int,
 Volume: int
 }
-
-
-DATABASE STRUCTURE AFTER PRE-PROCESSING
-
-processed data {
-Stock: varchar(6),
-High Price: int,
-Low Price: int,
-Close Price: int,
-Volume: int,
-P/E: int,
-P/B: int
-}
 """
 
-import yfinance as yf
-import sqlite3 as sq3
-import pandas as pd
-import os
-from preprocessing import preprocess_data
-
-# database path var
 DB_PATH = "databases/tradingbot.db"
 
+def preprocessor(ticker, data):
+    # given a ticker and its data
+    # sort the ticker by its timestamp
+    # drop all invalid data points (NaN, Infinity, None)
+    # back/forward fill all missing data points
 
-# gets yfinance data for a given list of tickers. returns multi-index dataframe
-def fetch_data(tickers):
-    data = yf.download(tickers,period='1y',interval='1d', group_by='ticker', auto_adjust=True)
-    data = data.dropna()
-
-    data = data.sort_index(axis=1, level=1)
-    data = data.sort_index()
-
-    return data
-
-# function that creates an sqlite table via sqlite3
-def create_table():
-    # ensure db and dirs exist before connecting
-    os.makedirs(os.path.dirname(DB_PATH), exist_ok = True)
-    con = sq3.connect(DB_PATH)
-    cur = con.cursor()
-
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS dataloader (
-        ticker TEXT,
-        timestamp TEXT,
-        open REAL,
-        high REAL,
-        low REAL,
-        close REAL,
-        volume INTEGER,
-        PRIMARY KEY (ticker, timestamp)
-        )
-        """)
-
-    con.commit()
-    con.close()
-
-# inserts ticker data into sqlite3 db
-def insert_data(ticker, df):
-    con = sq3.connect(DB_PATH)
-    cur = con.cursor()
-
-    # using INSERT OR IGNORE to ensure we aren't overwriting data we've already written
-    for index, row in df.iterrows():
-        cur.execute("""
-        INSERT OR IGNORE INTO dataloader (ticker, timestamp, open, high, low, close, volume)
-        VALUES (?,?,?,?,?,?,?)
-        """, (ticker, index.isoformat(), row['Open'], row['High'], row['Low'], row['Close'], int(row['Volume']))
-        )
-
-#        print(f"[{ticker:<4}]: Loaded: {row['Open']}, {row['High']}, {row['Low']}, {row['Close']}, {int(row['Volume'])}")
-
-    con.commit()
-    con.close()
-
-
-# takes a list of tickers and populates sqlite3 db with their information
+    # return data, where data is the sorted and filled dataframe
+    pass
+ 
 def dataloader(tickers):
-    create_table()
+    # download historical data with yf.download() in one-hour increments with a period of 1y, sorted by ticker
+    # for every ticker's data in the list, run the preprocessor on it
+        # after the data is preprocessed, insert it into the database
+    # return data, where data is a multi-index dataframe
+    pass
 
-    # if passed one ticker, turn it into a one-ticker list
-    if isinstance(tickers, str):
-        tickers = [ticker]
+def boostrap_dataloader(tickers):
+    # if tickers is a string, turn it into a list
+    # check to see if database exists. if not, make it
+        # check to see if table exists. if not, run the dataloader
+        # check to see if table has values for the specified time and date. if not, drop the table and run the dataloader
 
-    # sort alphabetically
-    tickers.sort()
-
-    data = fetch_data(tickers)
-
-    for ticker in tickers:
-        try:
-            # get a dataframe from a multiindex, or if it's already a dataframe (in the case that we have one ticker), just use that one
-            df = data[ticker] if isinstance(data.columns, pd.MultiIndex) else data
-
-            # add data for missing datapoints via backfilling
-            df.index = pd.to_datetime(df.index)
-            full_range = pd.date_range(start=df.index.min(), end=df.index.max(), freq='h')
-            df = df.reindex(full_range, method=None)
-            df.ffill(inplace=True)
-            df.bfill(inplace=True)
-
-            # insert data to sqlite
-            insert_data(ticker, df)
-
-            print(f"[{ticker:<4}]: Loaded data into SQLite.")
-
-        except Exception as ex:
-            print(f"[{ticker:<4}]: Failed to load data into SQLite. {ex}")
-            return None
-            
-    return data
+    # return data, where data is a multi-index dataframe
+    pass
