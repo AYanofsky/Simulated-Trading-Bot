4ee8aebaf1ab7d810de7149f57f467387227faa7
Andrew Yanofsky <andrewdyanofsky@gmail.com>
2025-04-09 01:50:42 -0400

    added debug lines

--- tradingbot/backtest.py
+++ tradingbot/backtest.py
@@ -119,6 +119,10 @@ def backtest(tickers, data, initial_balance=10000, stop_loss_percent=0.1029, tak
 
     returns = portfolio_history_df['portfolio_value'].pct_change().dropna()
 
+    print(f"[SYSTEM]: FINAL VALUE: ${final_value:,.2f}")
+    print(f"[SYSTEM]: PERCENT RETURN: {returns:,.2f}%")
+
+
     if len(returns) == 0:
         return 0.0, 0.0, 0.0, returns
 
